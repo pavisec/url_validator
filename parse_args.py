ce9ef72e9f200422528309530c902c0fedcbe8bf
pavisec <134921720+pavisec@users.noreply.github.com>
2023-05-31 12:05:30 +0300

    Minor code fixes

--- parse_args.py
+++ parse_args.py
@@ -4,7 +4,7 @@ import argparse
 def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("-u", "--url", required=True, help="URL of the website to check")
-    parser.add_argument("-t", "--timeout", help="Timeout for the request in seconds", type=int, default=5)
+    parser.add_argument("-t", "--timeout", help="Timeout for the request in seconds (default: 5)", type=int, default=5)
 
     args = parser.parse_args()
     return args.url, args.timeout
