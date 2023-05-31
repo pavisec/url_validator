0c006cdd474db80c302e5d5ba14dc73af128ff44
pavisec <134921720+pavisec@users.noreply.github.com>
2023-05-31 11:16:54 +0300

    Setup parser for command line arguments

---
+++ parse_args.py
@@ -0,0 +1,10 @@
+import argparse
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", "--url", required=True, help="URL of the website to check")
+    parser.add_argument("-t", "--timeout", help="Timeout for the request in seconds", type=int, default=5)
+
+    args = parser.parse_args()
+    return args.url, args.timeout
